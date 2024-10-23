f361855af7ca123596ee0740caba31683f1275b9
flashman <flashman@flashman.com>
2024-10-24 07:38:50 +0800

    feat: add a testcase to duplicate checker how to detect a file's change operation

--- java-functions/src/main/java/com/example/demo/mongodb/entity/FileInfo.java
+++ java-functions/src/main/java/com/example/demo/mongodb/entity/FileInfo.java
@@ -3,17 +3,14 @@ package com.example.demo.mongodb.entity;
 import java.util.Date;
 import java.util.Map;
 
+import lombok.*;
 import org.springframework.data.annotation.Id;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ToString
 public class FileInfo {
     @Id
     public String uuid;
--- java-functions/src/test/java/com/example/demo/mongodb/EliminateDuplicateTest.java
+++ java-functions/src/test/java/com/example/demo/mongodb/EliminateDuplicateTest.java
@@ -4,12 +4,19 @@ import com.example.demo.lucenesearch.Utils;
 import com.example.demo.mongodb.entity.FileInfo;
 import com.example.demo.mongodb.repository.FileInfoRepository;
 import jakarta.annotation.Resource;
+import org.apache.commons.io.FileUtils;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
 
 /**
  * TODO: function description
@@ -18,7 +25,8 @@ import java.io.File;
  */
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = MongoDbTestConfig.class)
-@TestPropertySource(locations = {"classpath*:/application-ut.properties"}) // TODO: it will work fine if the properties file is placed here
+@TestPropertySource(locations = {"classpath*:/application-ut.properties"})
+// TODO: it will work fine if the properties file is placed here
 //@SpringBootTest(classes = MongoDbTestConfig.class)
 // DONE: java.lang.NoSuchMethodError: 'org.junit.jupiter.api.extension.ExecutableInvoker org.junit.jupiter.api.extension.ExtensionContext.getExecutableInvoker()' "Java, Spring Boot & JUnit 5 NoSuchMethod error on getExecutableInvoker - Stack Overflow" https://stackoverflow.com/questions/78381933/java-spring-boot-junit-5-nosuchmethod-error-on-getexecutableinvoker
 public class EliminateDuplicateTest {
@@ -26,25 +34,67 @@ public class EliminateDuplicateTest {
 
     @Resource
     FileInfoRepository repository;
+    SoftAssertions softly = new SoftAssertions();
 
-    @org.junit.Test
-    public void testFindByHash() {
-        repository.deleteAll();
+    @Before
+    public void beforeAll() {
 
-        FileInfo fileInfoEntity = Utils.buildFileInfoEntity(Utils.getFileFrom(testFileRelativePath), 0);
-        FileInfo saved = repository.save(fileInfoEntity);
-        System.out.println("after saving, uuid: " + saved.getUuid() + ",fileName: " + saved.getFileName() + ", path: " + saved.getPath());
+    }
 
-        FileInfo fileInfo = repository.findByHash(saved.getHash());
-        System.out.println("after calling findByHash, uuid: " + fileInfo.getUuid() + ",fileName: " + fileInfo.getFileName() + ", path: " + fileInfo.getPath());
+    @After
+    public void afterAll() {
+        softly.assertAll();
     }
 
     @org.junit.Test
-    public void testExtractingMetaDataFromFile() {
-        File testFile = new File(Utils.getRealPath(testFileRelativePath));
+    public void test_insertFileInfoAndFindByHash() {
+        repository.deleteAll();
+
+        FileInfo fileInfoEntity = Utils.buildFileInfoEntity(Utils.getFileFrom(testFileRelativePath), 0);
+        FileInfo savedFileInfoEntity = repository.save(fileInfoEntity);
+        System.out.println("after saving, uuid: " + savedFileInfoEntity.getUuid() + ",fileName: " + savedFileInfoEntity.getFileName() + ", path: " + savedFileInfoEntity.getPath());
+
+        FileInfo foundFileInfoEntity = repository.findByHash(savedFileInfoEntity.getHash());
+        System.out.println("after calling findByHash, uuid: " + foundFileInfoEntity.getUuid() + ",fileName: " + foundFileInfoEntity.getFileName() + ", path: " + foundFileInfoEntity.getPath());
+
+        System.out.println("\n\nafter saving, savedFileInfoEntity: " + savedFileInfoEntity + ", foundFileInfoEntity: " + foundFileInfoEntity);
+    }
+
+    @org.junit.Test
+    public void test_extractMetaDataFromFile() {
+        File testFile = Utils.getFileFrom(testFileRelativePath);
         Utils.FileInfoExtractor.FileMetadata fileMetadata = Utils.FileInfoExtractor.extractMetaInfoFrom(testFile);
         System.out.println("metadata: " + fileMetadata);
         String fileMD5 = Utils.getFileMD5(testFile);
         System.out.println("fileMD5: " + fileMD5);
+    }
+
+    @org.junit.Test
+    public void test_detectFileChangeOperation() {
+        try {
+            File testedFile = Paths.get(Utils.getRealPath("/"), "testedFileForChangeOperation.txt").toFile();
+            System.out.println("testedFile: " + testedFile.getAbsolutePath());
+            FileUtils.touch(testedFile);
+
+            FileUtils.write(testedFile, "test", StandardCharsets.UTF_8);
+
+            Utils.FileInfoExtractor.FileMetadata fileMetadata = Utils.FileInfoExtractor.extractMetaInfoFrom(testedFile);
+            System.out.println("metadata: " + fileMetadata);
+            String fileMD5 = Utils.getFileMD5(testedFile);
+            System.out.println("fileMD5: " + fileMD5);
+
+            Utils.FileInfoExtractor.FileBasicInfo fileBasicInfo1 = Utils.FileInfoExtractor.extractBasicInfoFrom(testedFile);
+            System.out.println("fileBasicInfo1: " + fileBasicInfo1);
+
+            String readFileToString = FileUtils.readFileToString(testedFile, StandardCharsets.UTF_8);
+            System.out.println("read char length: " + readFileToString.length());
+
+            Utils.FileInfoExtractor.FileBasicInfo fileBasicInfo2 = Utils.FileInfoExtractor.extractBasicInfoFrom(testedFile);
+            System.out.println("fileBasicInfo2: " + fileBasicInfo2);
+
+            softly.assertThat(fileBasicInfo1).as("ASSERT: reading operation will not change basic info of a file").isEqualTo(fileBasicInfo2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
